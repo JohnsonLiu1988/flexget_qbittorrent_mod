3cfe6008e466a0cb0cee7bf38fa1f3fd9a5922e3
Johnson Liu <lwj673404059@163.com>
2023-09-23 15:48:03 +0800

    Update hddolby.py

--- ptsites/sites/hddolby.py
+++ ptsites/sites/hddolby.py
@@ -2,6 +2,7 @@ from typing import Final
 
 from ..base.reseed import ReseedCookie
 from ..schema.nexusphp import AttendanceHR
+from ..utils import net_utils
 
 
 class MainClass(AttendanceHR, ReseedCookie):
@@ -11,3 +12,15 @@ class MainClass(AttendanceHR, ReseedCookie):
         'share_ratio': [4, 5.5],
         'days': [112, 336]
     }
+    
+    @property
+    def details_selector(self) -> dict:
+        selector = super().details_selector
+        net_utils.dict_merge(selector, {
+            'details': {
+                'points': {
+                    'regex': (r'(\u9b54\u529b\u503c|\u9b54\u529b|Bonus|Bônus|\u9cb8\u5e01).*?([\d,.]+)', 2)
+                },
+            }
+        })
+        return selector
